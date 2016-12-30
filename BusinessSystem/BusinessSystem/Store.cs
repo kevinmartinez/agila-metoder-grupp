391efc8a3956f87944aa4f7ac88da48ad422e706
Stefan <stefan.a.lind@comhem.se>
2016-12-30 18:07:29 +0100

    Store och StoreInterface tillagda.

---
+++ BusinessSystem/BusinessSystem/Store.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessSystem
+{
+
+    //===========================================================================================
+    // Product Class.
+    //===========================================================================================
+    public class Product
+    {
+        private string _number;
+        private string _name;
+        private double _price;
+        private int _quantity;
+
+        public string number { get { return _number; } }
+        public string name { get { return _name; } }
+        public double price { get { return _price; } }
+        public int quantity { get { return _quantity; } }
+
+        //--- Constructor ---
+        public Product(string number, string name, double price, int quantity)
+        {
+            _number = number;
+            _name = name;
+            _price = price;
+            _quantity = quantity;
+        }
+    }
+
+
+    //===========================================================================================
+    // Generic Store class.
+    //===========================================================================================
+    public class Store<T> : IEnumerable where T : Product
+    {
+        public List<T> products = new List<T>();
+
+
+        //--- Enumerator. ---
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = 0; i < products.Count; i++)
+            {
+                yield return products[i];
+            }
+        }
+
+
+        //--- Add product. ---
+        public bool AddProduct(T product)
+        {
+            //--- Make sure the artikel not already in the Store. ---
+            if (GetProductByNumber(product.number) == null & GetProductBylName(product.name) == null)
+            {
+                products.Add(product);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+
+        //--- Get product by number. ---
+        public Product GetProductByNumber(string number)
+        {
+            //--- Select products from store that corresponds to the given number (either zero or one product). ---
+            Product[] productGet = products.Where(item => item.number == number).ToArray();
+
+            if (productGet.Length > 0)
+            {
+                return productGet[0];
+            }
+            else
+            {
+                return null;
+            }
+
+        }
+
+
+        //--- Get product by name. ---
+        public Product GetProductBylName(string name)
+        {
+            //--- Select products from store that corresponds to the given name (either zero or one product). ---
+            Product[] productGet = products.Where(item => item.name == name).ToArray();
+
+            if (productGet.Length > 0)
+            {
+                return productGet[0];
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+    }
+
+}
---
+++ BusinessSystem/BusinessSystem/StoreInterface.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessSystem
+{
+    // Class Store interface. Channel to Store class. ---
+    public class StoreInterface
+    {
+        Store<Product> store = new Store<Product>();
+
+
+        //==============================================================================================
+        // Add product.
+        // Input parameters are all string values from input source (console).
+        // The parameters will be converted to respective format.
+        // Returns an empty string (no error message) if added ok. 
+        //==============================================================================================
+        public string AddProduct(string number, string name, string price, string quantity)
+        {
+            string errorMessage = "";
+            double priceDbl;    //- Price as double
+            int quantityInt;    //- Quantity as integer
+
+            //--- Make sure number and name not an empty string, price is double, quantity is integer. ---
+            bool formatOk = ((number != null & number != "") & (name != null & name != "")
+                & double.TryParse(price, out priceDbl) & int.TryParse(quantity, out quantityInt));
+
+            if (formatOk)
+            {
+                Product product = new Product(number, name, priceDbl, quantityInt);
+
+                //--- Add product if not already in the store (number or name must not exists). ---
+                if (!store.AddProduct(product))
+                {
+                    errorMessage = "Product already in store.";
+                }
+
+            }
+            else
+            {
+                errorMessage = "Illegal format. Number, name must not be empty, price needs to be a number (i.e. 12,50), quantity an integer number.";
+            }
+
+            return errorMessage;
+        }
+    }
+
+
+
+
+
+}
