73e0cadb77c74d8dd66d5cd4ad99d6f125a802ff
Stefan <stefan.a.lind@comhem.se>
2016-12-30 13:45:10 +0100

    BusinessSystem_Test omdöpt till BusinessSystem

--- BusinessSystem/BusinessSystem/Program.cs
+++ BusinessSystem/BusinessSystem/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BusinessSystem_Test {
+namespace BusinessSystem {
     class Program {
         static void Main(string[] args) {
             BusinessSystemUi ui = new BusinessSystemUi();
